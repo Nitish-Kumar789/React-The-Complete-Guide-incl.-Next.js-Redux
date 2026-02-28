a31286bea100aea12227f35e6701dcf01788c58f
Nitish-Kamath <nitishkamath63590@gmail.com>
2026-02-28 23:46:05 +0530

    Handling Loading States

--- React Code/HTTP Requests/src/components/AvailablePlaces.jsx
+++ React Code/HTTP Requests/src/components/AvailablePlaces.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setisFetching] = useState(false);
+
   const [AvailablePlaces, setAvailablePlaces] = useState([]);
 
   // useEffect(() => {
@@ -15,10 +17,12 @@ export default function AvailablePlaces({ onSelectPlace }) {
   // }, []);
 
   useEffect(() => {
+    setisFetching(true);
     async function fetchPlaces() {
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setisFetching(false);
     }
     fetchPlaces();
   }, []);
@@ -27,6 +31,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={AvailablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
