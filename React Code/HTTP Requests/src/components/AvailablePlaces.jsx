91d5df46ca154c94564d2f10f758046b66bc0991
Nitish-Kamath <nitishkamath63590@gmail.com>
2026-03-01 00:07:00 +0530

    Handling HTTP Errors

--- React Code/HTTP Requests/src/components/AvailablePlaces.jsx
+++ React Code/HTTP Requests/src/components/AvailablePlaces.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setisFetching] = useState(false);
-
   const [AvailablePlaces, setAvailablePlaces] = useState([]);
-
+  const [error, setError] = useState();
   // useEffect(() => {
   //   fetch("http://localhost:3000/places")
   //     .then((reponse) => {
@@ -18,14 +18,32 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
   useEffect(() => {
     setisFetching(true);
+
     async function fetchPlaces() {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message ||
+            "Could not fetch places, please try again later...",
+        });
+      }
+
       setisFetching(false);
     }
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occured!" message={error.message} />;
+  }
 
   return (
     <Places
