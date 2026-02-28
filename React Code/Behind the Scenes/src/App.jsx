81fee15417ed1c39e4ab54fce5162b091cb57257
Nitish-Kamath <nitishkamath63590@gmail.com>
2026-02-28 15:45:17 +0530

    Understanding the useMemo() Hook

--- React Code/Behind the Scenes/src/App.jsx
+++ React Code/Behind the Scenes/src/App.jsx
@@ -4,6 +4,7 @@ import Counter from "./components/Counter/Counter.jsx";
 import Header from "./components/Header.jsx";
 import ConfigureCounter from "./components/Counter/ConfigureCounter.jsx";
 import { log } from "./log.js";
+
 function App() {
   log("<App /> rendered");
 
